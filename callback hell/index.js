14973005ef066c91bd6ab4eb3d536a0f8bf051b1
kashish <hsihsak2710@gmail.com>
2025-05-14 12:20:18 +0530

    Your commit message

---
+++ callback hell/index.js
@@ -0,0 +1,37 @@
+
+//function without any call back
+let data;
+function internetspeed(data){
+    let random=Math.floor(Math.random()*10)+1;
+if(random>4){
+    console.log(`this data:${data} saved in DB .`)
+}
+else{
+    console.log("your data not saved in DB. ")
+}
+}
+internetspeed("hello world");
+
+
+//function with callback 
+
+function internetspeed(data,success,failure){
+    let random=Math.floor(Math.random()*10)+1;
+if(random>4){
+    success();
+}
+else{
+   failure();
+}
+
+}
+internetspeed("data1 ",()=>{
+    console.log("data1 saved !")
+    internetspeed("data2",()=>{
+        console.log("data2 saved");
+    },()=>{
+        console.log("data2 not saved");
+    })
+},()=>{
+    console.log("data1 not saved!")
+})
